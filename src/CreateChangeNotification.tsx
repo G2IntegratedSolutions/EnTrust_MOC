f45f5342f38257670d8deafd232cb33aaf7f463f
RiverMadsen <rtaig@entrustsol.com>
2024-02-13 13:07:03 -0800

    Added ability to create random data

--- src/CreateChangeNotification.tsx
+++ src/CreateChangeNotification.tsx
@@ -47,9 +47,59 @@ const CreateChangeNotification: React.FC = () => {
         });
     }, []);
 
+    const onCreateRandomCN = async () => {
+        setMocNumber(generateRandomString(8));
+        //const randomDate = getRandomDate();
+        setDateOfPublication(getRandomDate()); // Convert randomDate to string
+        setDateOfCreation(getRandomDate()); // Convert randomDate to string
+        setTimeOfImplementation(getRandomTime());
+        // debugger
+        setSelectedChangeCategory(changeCategories[Math.floor(Math.random() * changeCategories.length)]);
+        setChangeType(getRandomText(16));
+        setChangeTopic(getRandomText(48));
+        setShortReasonForChange(getRandomText(100));
+        setDescriptionOfChange(getRandomText(300));
+        setImpacts(getRandomText(50));
+        setRequiredDateOfCompletion(getRandomDate());
+        setNotes(getRandomText(200));
+        setAttachments([]);
+
+        const numGroupsToSelect = Math.floor(Math.random() * (groupsForOrganization.length + 1));
+        const xselectedGroups : Group[] = [];
+
+        for (let i = 0; i < numGroupsToSelect; i++) {
+            let randomIndex = Math.floor(Math.random() * groupsForOrganization.length);
+            let randomGroup = groupsForOrganization[randomIndex];
+
+            // Ensure the group isn't already selected
+            if (!xselectedGroups.some(group => group.id === randomGroup.id)) {
+                xselectedGroups.push(randomGroup);
+            } else {
+                // Decrement the counter to try again
+                i--;
+            }
+        }
+
+        setSelectedGroups(xselectedGroups);
+    }
+    function getRandomTime() {
+        const hours = ("0" + Math.floor(Math.random() * 24)).slice(-2);
+        const minutes = ("0" + Math.floor(Math.random() * 60)).slice(-2);
+        return `${hours}:${minutes}`;
+    }
+    function getRandomDate() {
+        const today = new Date();
+        const futureDate = new Date();
+        futureDate.setDate(today.getDate() + Math.floor(Math.random() * 365));
+
+        const year = futureDate.getFullYear();
+        const month = ("0" + (futureDate.getMonth() + 1)).slice(-2); // Months are 0-indexed in JavaScript
+        const day = ("0" + futureDate.getDate()).slice(-2);
+
+        return `${year}-${month}-${day}`;
+    }
     const onCreateChangeNotification = async (e: FormEvent) => {
         e.preventDefault();
-        debugger;
         try {
             const cn = {
                 mocNumber,
@@ -59,7 +109,7 @@ const CreateChangeNotification: React.FC = () => {
                 category: selectedChangeCategory,
                 type: changeType,
                 topic: changeTopic,
-                groups:selectedGroups,
+                groups: selectedGroups,
                 shortReasonForChange,
                 descriptionOfChange,
                 impacts,
@@ -83,6 +133,23 @@ const CreateChangeNotification: React.FC = () => {
             setSelectedGroups(prevGroups => prevGroups.filter(g => g.id !== group.id));
         }
     };
+
+    const getRandomText = (length: number) => {
+        const longString = "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way. When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then"
+
+        let start = Math.floor(Math.random() * (longString.length - length - 1));
+
+        // Find the next space after the random start index
+        while (longString[start] !== ' ' && start < longString.length) {
+            start++;
+        }
+
+        // Start from the character after the space
+        start++;
+
+        return longString.substring(start, start + length);
+    }
+
     return (
         <div>
             <h2>Create Change Notice</h2>
@@ -117,6 +184,7 @@ const CreateChangeNotification: React.FC = () => {
                         type="date"
                         className="form-control"
                         id="dateOfPublication"
+                        value={dateOfPublication}
                         onChange={(e) => setDateOfPublication(e.target.value)}
                     />
                 </div>
@@ -126,12 +194,15 @@ const CreateChangeNotification: React.FC = () => {
                         type="time"
                         className="form-control"
                         id="timeOfImplementation"
+                        value={timeOfImplementation}
                         onChange={(e) => setTimeOfImplementation(e.target.value)}
                     />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="category" className="form-label">Category</label>
-                    <select className='form-control' onChange={(e) => setSelectedChangeCategory(e.target.value)}>
+                    <select className='form-control'
+                        value={selectedChangeCategory}
+                        onChange={(e) => setSelectedChangeCategory(e.target.value)}>
                         {changeCategories.map((cat, index) => (
                             <option key={index} value={cat}>{cat}</option>
                         ))}
@@ -142,7 +213,8 @@ const CreateChangeNotification: React.FC = () => {
                     <input
                         type="text"
                         className="form-control"
-                        id="type"
+                        value={changeType}
+                        id="changeType"
                         onChange={(e) => setChangeType(e.target.value)}
                     />
                 </div>
@@ -151,6 +223,7 @@ const CreateChangeNotification: React.FC = () => {
                     <input
                         type="text"
                         className="form-control"
+                        value={changeTopic}
                         id="topic"
                         onChange={(e) => setChangeTopic(e.target.value)}
                     />
@@ -167,6 +240,7 @@ const CreateChangeNotification: React.FC = () => {
                                         value={group.name}
                                         id={group.id}
                                         onChange={(e) => handleCheckboxChange(e, group)}
+                                        checked={selectedGroups.some(selectedGroup => selectedGroup.id === group.id)}
                                     />
                                     <label style={{ marginLeft: '4px' }} className="form-check-label" htmlFor={group.id}>
                                         {group.name}
@@ -181,6 +255,7 @@ const CreateChangeNotification: React.FC = () => {
                     <input
                         type="text"
                         className="form-control"
+                        value={shortReasonForChange}
                         onChange={(e) => setShortReasonForChange(e.target.value)}
                         id="shortReasonForChange" />
                 </div>
@@ -188,6 +263,7 @@ const CreateChangeNotification: React.FC = () => {
                     <label htmlFor="descriptionOfChange" className="form-label">Description Of Change</label>
                     <textarea
                         className="form-control"
+                        value={descriptionOfChange}
                         id="descriptionOfChange"
                         onChange={(e) => setDescriptionOfChange(e.target.value)}
                     />
@@ -197,6 +273,7 @@ const CreateChangeNotification: React.FC = () => {
                     <input
                         type="text"
                         className="form-control"
+                        value={impacts}
                         id="impacts"
                         onChange={(e) => setImpacts(e.target.value)}
                     />
@@ -206,6 +283,7 @@ const CreateChangeNotification: React.FC = () => {
                     <input
                         type="date"
                         className="form-control"
+                        value={requiredDateOfCompletion}
                         id="requiredDateOfCompletion"
                         onChange={(e) => setRequiredDateOfCompletion(e.target.value)}
                     />
@@ -215,6 +293,7 @@ const CreateChangeNotification: React.FC = () => {
                     <input
                         type="text"
                         className="form-control"
+                        value={notes}
                         id="notes"
                         onChange={(e) => setNotes(e.target.value)}
                     />
@@ -232,6 +311,8 @@ const CreateChangeNotification: React.FC = () => {
                     />
                 </div>
                 <button type="button" onClick={onCreateChangeNotification} className="btn btn-primary">Create Change Notice</button>
+                <br />
+                <button type="button" onClick={onCreateRandomCN} className="btn btn-primary">!DEBUG! Create RANDOM Change Notice</button>
             </form>
         </div>
     );
